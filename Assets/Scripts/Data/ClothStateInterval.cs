296aea0d2ecc958ac32aa439e9931210d01815d8
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-11 13:10:35 +0700

    Add ClothInterval

---
+++ Assets/Scripts/Data/ClothStateInterval.cs
@@ -0,0 +1,19 @@
+public class ClothStateInterval : BaseSingleton<ClothStateInterval>
+{
+    private float wet = 4;
+    private float mostlyWet = 3;
+    private float mostlyDry = 2;
+    private float dry = 1;
+
+    public float GetInterval(ClothState state)
+    {
+        switch(state)
+        {
+            case ClothState.Wet: return wet;
+            case ClothState.MostlyWet: return mostlyWet;
+            case ClothState.MostlyDry: return mostlyDry;
+            case ClothState.Dry: return dry;
+            default: return 0;
+        }
+    }
+}
