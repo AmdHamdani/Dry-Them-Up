2312d494ab28ffce726a38144bc82cd2732e8fa5
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-12 00:00:44 +0700

    Add Weather data

---
+++ Assets/Scripts/Data/Weather.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Weather", menuName = "Game/New Weather")]
+public class Weather : ScriptableObject
+{
+    public string weatherName;
+    [TextArea]
+    public List<string> weatherText;
+
+    public string GetText()
+    {
+        return weatherText[Random.Range(0, weatherText.Count)];
+    }
+}
