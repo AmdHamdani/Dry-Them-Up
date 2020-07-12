2561a6b03cbce656d3e6e75e29f3fd4e8a559911
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-12 15:18:49 +0700

    Add StateController

---
+++ Assets/Scripts/Game/StateController.cs
@@ -0,0 +1,11 @@
+﻿public class StateController<T> where T : System.Enum
+{
+
+    public T CurrentState { get; private set; }
+
+    public void ChangeState(T state)
+    {
+        CurrentState = state;
+    }
+
+}
