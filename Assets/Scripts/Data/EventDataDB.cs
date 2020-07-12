34c5464938a119ff8cc27e1fcf1192a3efbab2a3
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-12 15:18:40 +0700

    Add EventDataDB

---
+++ Assets/Scripts/Data/EventDataDB.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventDataDB", menuName = "Game/EventDataDB")]
+public class EventDataDB : ScriptableObject
+{
+
+    public int totalCloth = 0;
+    public List<EventData> database = new List<EventData>();
+
+    public static EventDataDB Load(string fileName = "Events/EventDataDB")
+    {
+        return Resources.Load<EventDataDB>(fileName);
+    }
+
+    public EventData GetEventData()
+    {
+        return database[Random.Range(0, database.Count)];
+    }
+}
