28969c702ed838027411e61cdf263d5c0beacc3f
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-12 08:03:16 +0700

    Add EventData

---
+++ Assets/Scripts/Data/EventData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventData : ScriptableObject
+{
+
+    public string eventName;
+    [TextArea]
+    public string eventText;
+    public OptionStr choiceOne;
+    public OptionStr ChoiceTwo;
+}
+
+[System.Serializable]
+public class OptionStr
+{
+    public string choice;
+    public string result;
+}
