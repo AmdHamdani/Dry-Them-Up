cc4be8a59e66ccea8fa36bbf6b76e059db5b5dc3
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-14 07:46:30 +0700

    Add ShowPanel

--- Assets/Scripts/Game/GameEventSystem.cs
+++ Assets/Scripts/Game/GameEventSystem.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameEventSystem : SingletonBehaviour<GameEventSystem>
 {
@@ -15,16 +14,14 @@ public class GameEventSystem : SingletonBehaviour<GameEventSystem>
 
     private void Awake()
     {
-        components = FindObjectsOfType<DragAndDropComponent>();    
+        components = FindObjectsOfType<DragAndDropComponent>();
     }
 
     private void Start()
     {
         database = EventDataDB.Load();
 
-        InGameUI.Instance.skyText.text = string.Empty;
-        InGameUI.Instance.leftChoiceText.text = string.Empty;
-        InGameUI.Instance.rightChoiceText.text = string.Empty;
+        ClearUIText();
     }
 
     private void Update()
@@ -33,12 +30,11 @@ public class GameEventSystem : SingletonBehaviour<GameEventSystem>
         {
             SetEventData();
             isEventActive = true;
-            Debug.Log("SET EVENT DATA");
         }
 
-        if(!isFinish && IsTrue(ClothState.Dry))
+        if (!isFinish && IsTrue(ClothState.Dry))
         {
-            GetResult();
+            isFinish = true;
         }
     }
 
@@ -50,18 +46,22 @@ public class GameEventSystem : SingletonBehaviour<GameEventSystem>
         if (!string.IsNullOrEmpty(currentEvent.choiceOne.choice))
         {
             InGameUI.Instance.leftChoiceText.text = "[ " + currentEvent.choiceOne.choice;
-            InGameUI.Instance.leftChoice.onClick.AddListener(() => {
+            InGameUI.Instance.leftChoice.onClick.AddListener(() =>
+            {
                 result = currentEvent.choiceOne.result;
-                //Debug.Log("RESULT : " + currentEvent.choiceOne.result);
+                ShowPanel(currentEvent.choiceOne.detail);
+                ClearUIText();
             });
-    }
+        }
 
         if (!string.IsNullOrEmpty(currentEvent.choiceTwo.choice))
         {
             InGameUI.Instance.rightChoiceText.text = currentEvent.choiceTwo.choice + " ]";
-            InGameUI.Instance.rightChoice.onClick.AddListener(() => {
+            InGameUI.Instance.rightChoice.onClick.AddListener(() =>
+            {
                 result = currentEvent.choiceTwo.result;
-                //Debug.Log("RESULT : " + currentEvent.choiceTwo.result);
+                ShowPanel(currentEvent.choiceTwo.detail);
+                ClearUIText();
             });
         }
     }
@@ -69,8 +69,9 @@ public class GameEventSystem : SingletonBehaviour<GameEventSystem>
     private bool IsTrue(ClothState threshold)
     {
         int counter = 0;
-        foreach(var item in components) {
-            if((int)item.state >= (int)threshold)
+        foreach (var item in components)
+        {
+            if ((int)item.state >= (int)threshold)
             {
                 counter++;
             }
@@ -84,7 +85,41 @@ public class GameEventSystem : SingletonBehaviour<GameEventSystem>
         switch (result)
         {
             case OptionResult.Num: score += components.Length; break;
+            case OptionResult.RandomNum: 
+                int random = Random.Range(0, components.Length);
+                int loop = components.Length - random;
+                score += random;
+                for(int i = 0; i < loop; i++)
+                {
+                    components[i].gameObject.SetActive(false);
+                }
+                break;
         }
+        Debug.Log(components.Length);
+        Debug.Log("Score : " + score);
+    }
+
+    private void ClearUIText()
+    {
+        InGameUI.Instance.skyText.text = string.Empty;
+        InGameUI.Instance.leftChoiceText.text = string.Empty;
+        InGameUI.Instance.rightChoiceText.text = string.Empty;
+    }
+
+    private void ShowPanel(string detail)
+    {
+        InGameUI.Instance.panel.SetActive(true);
+        InGameUI.Instance.panelImage.color = Color.white;
+        InGameUI.Instance.panelText.text = detail;
+
+        StartCoroutine(Fun.WaitFor(1f, () => {
+            InGameUI.Instance.panel.SetActive(false);
+            foreach (var item in components)
+            {
+                item.state = ClothState.Dry;
+            }
+            GetResult();
+        }));
     }
 
 }
