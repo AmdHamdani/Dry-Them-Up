4a4c69ee06df48dc00117a5f1fa4205761b9be98
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-17 07:26:42 +0700

    Add SkyColor

---
+++ Assets/Scripts/Data/SkyColor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkyColor", menuName = "Game/New Sky Color")]
+public class SkyColor : ScriptableObject
+{
+
+    public Color sunny;
+    public Color cloudy;
+    public Color windy;
+
+    public static SkyColor Instance
+    {
+        get
+        {
+            return Resources.Load<SkyColor>("Weather/SkyColor");
+        }
+    }
+}
