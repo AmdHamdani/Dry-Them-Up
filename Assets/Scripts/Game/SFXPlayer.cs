e9a1e129914d7e1367b9d5c2867031d4591d6a14
AmdHamdani <Hamdaniryuuken@gmail.com>
2020-07-18 21:59:34 +0700

    Add SFXPlayer

---
+++ Assets/Scripts/Game/SFXPlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SFXPlayer : SingletonBehaviour<SFXPlayer>
+{
+
+    public AudioClip clip;
+
+    private AudioSource player;
+
+    private void Start() {
+        player = gameObject.AddComponent<AudioSource>();
+        player.clip = clip;
+    }
+
+    public void Play() {
+        player.Play();
+    }
+
+}
